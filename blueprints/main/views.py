a52624b8cca197958382c5b826e71341fa68ec6f
Jai Sawhney <jai.sawhney1@gmail.com>
2021-12-01 16:51:09 -0800

    Start html

--- blueprints/main/views.py
+++ blueprints/main/views.py
@@ -6,3 +6,8 @@ view = Blueprint("main", __name__, template_folder="templates")
 @view.route("/")
 def index():
     return render_template("index.html")
+
+
+@view.route("/dashboard")
+def show_dashboard():
+    return render_template("dashboard.html")
